9ea70345d175c1c28b4cde5c855534bb8397c9c4
Merikrotti <merikrotti@pm.me>
2021-11-03 14:35:48 +0200

    step 4

--- osa1/kurssitiedot/step1/src/App.js
+++ osa1/kurssitiedot/step1/src/App.js
@@ -2,24 +2,25 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course}/>
-      <Content contents={[part1, part2, part3]}/>
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content contents={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -30,14 +31,22 @@ const Header = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    <p>Number of exercises {totalCounter(props.parts)}</p>
   )
+}
+
+const totalCounter = (parts) => {
+  let count = 0;
+  for(let i = 0; i < parts.length; i++) {
+    count += parts[i].exercises;
+  }
+  return count;
 }
 
 const Content = (props) => {
   return(<div>
     {props.contents.map((item, index) => {
-      return (<Part key={item} part={item.name} exercise={item.exercises} />)
+      return (<Part key={item.name} part={item.name} exercise={item.exercises} />)
     })}
     </div>
   )
